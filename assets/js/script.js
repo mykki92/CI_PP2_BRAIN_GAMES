9b6359c90509aa67121f8820109e62d4a5240776
mykki92 <mike2108@hotmail.co.uk>
2023-04-02 14:40:05 +0000

    Write javasript functions to handle card flip

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+const cards = document.querySelectorAll('.game-card');
+
+//card flip
+let hasFlippedCard = false;
+let firstCard, secondCard;
+let lockBoard = false;
+
+function flipCard () {
+    if (lockBoard) return;
+    if (this === firstCard) return;
+    this.classList.add('flip');
+
+    if (!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
+   }
+
+   secondCard = this;
+
+   checkForMatch();
+}
