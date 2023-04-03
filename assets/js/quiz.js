13414501b2aa5a9b3bad94a9122a7a8cdddebda3
mykki92 <mike2108@hotmail.co.uk>
2023-04-03 15:20:25 +0000

    Declare startQuiz function that displays questions and answer options

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -60,3 +60,68 @@ let questions = [
     },
 ]
 
+restartButton.addEventListener('click', restart);
+prevButton.addEventListener('click', prev);
+nextButton.addEventListener('click', next);
+submitButton.addEventListener('click', submit);
+
+function startQuiz() {
+    currentQuestion = 0;
+    questionText.innerHTML = questions[currentQuestion].question;
+    answerButtonA.innerHTML = questions[currentQuestion].answers[0].option;
+    answerButtonA.onclick = () => {
+        let ano = 0;
+        if(questions[currentQuestion].answers[ano].answer){
+            if(score<5){
+                score++;
+            }
+        }
+        playerScore.innerHTML = score;
+        if(currentQuestion<4){
+            next();
+        }
+    }
+    answerButtonB.innerHTML = questions[currentQuestion].answers[1].option;
+    answerButtonB.onclick = () => {
+        let ano = 1;
+        if(questions[currentQuestion].answers[ano].answer){
+            if(score<5){
+                score++;
+            }
+        }
+        playerScore.innerHTML = score;
+        if(currentQuestion<4){
+            next();
+        }
+    }
+    answerButtonC.innerHTML = questions[currentQuestion].answers[2].option;
+    answerButtonC.onclick = () => {
+        let ano = 2;
+        if(questions[currentQuestion].answers[ano].answer){
+            if(score<5){
+                score++;
+            }
+        }
+        playerScore.innerHTML = score;
+        if(currentQuestion<4){
+            next();
+        }
+    }
+    answerButtonD.innerHTML = questions[currentQuestion].answers[3].option;
+    answerButtonD.onclick = () => {
+        let ano = 3;
+        if(questions[currentQuestion].answers[ano].answer){
+            if(score<5){
+                score++;
+            }
+        }
+        playerScore.innerHTML = score;
+        if(currentQuestion<4){
+            next();
+        }
+    }
+    prevButton.classList.add('hide');
+}
+
+startQuiz();
+
