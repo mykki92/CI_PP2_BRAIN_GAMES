492977c71ff7529afa79c4092850b3c5eea114ac
mykki92 <mike2108@hotmail.co.uk>
2023-04-03 13:06:15 +0000

    Add game questions to js

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,2 +1,51 @@
 let questionContainer = document.getElementById("question");
 let submitButton = document.getElementById("submit-button");
+let resultsContainer = document/getElementById("results");
+
+const quizQuestions = [
+    {
+        //Question 1
+        question: "What is the only continent on Earth that does not have a desert?",
+        a: "Asia",
+        b: "Europe",
+        c: "Antarctica",
+        d: "South America",
+        answer: "Europe"
+    },
+    {
+        //Question 2
+        question: "Which is the hottest planet in the Milky Way?",
+        a: "Saturn",
+        b: "Jupiter",
+        c: "Venus",
+        d: "Mars",
+        answer: "Venus"
+    },
+    {
+        //Question 3
+        question: "Who was the Ancient Greek God of the Sun?",
+        a: "Apollo",
+        b: "Hermes",
+        c: "Dionysus",
+        d: "Ares",
+        answer: "Apollo"
+    },
+    {
+        //Question 4
+        question: "The Pantheon is located in which historic city?",
+        a: "Athens",
+        b: "Cairo",
+        c: "Istanbul",
+        d: "Rome",
+        answer: "Rome"
+    },
+    {
+        //Question 5
+        question: "How many hearts does an octopus have?",
+        a: "1",
+        b: "2",
+        c: "3",
+        d: "76",
+        answer: "3"
+    },
+];
