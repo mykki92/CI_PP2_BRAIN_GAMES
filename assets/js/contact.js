916f8d64fc1419545b5d1ef35c162be36f9ea092
mykki92 <mike2108@hotmail.co.uk>
2023-04-08 14:53:32 +0000

    Add text area to contact page and finalise page structure and css

---
+++ assets/js/contact.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        emailjs.sendForm('contact_service', 'contact_form', this)
+            .then(function() {
+                console.log('SUCCESS!');
+            }, function(error) {
+                console.log('FAILED...', error);
+            });
+    });
+}
